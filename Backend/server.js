54dd72a08f64ef507001aa025c7200ea060e3c1b
Theekshana Chathuranga <theekshanacnb@gmail.com>
2025-03-30 11:11:40 +0530

    Add room routes import to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import roomRoutes from "./routes/rooms.js";
 
 dotenv.config();
 
