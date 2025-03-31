20dfdb649b438f567e36da15ee82606021339ecc
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2025-03-31 13:50:43 +0530

    Customer When clicking the <Book Your Stay> go toCustomer dashBoard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Booking from "./pages/Booking";
+
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/booking" element={<Booking />} />
           
         </Routes>
       </div>
---
+++ Frontend/src/pages/Booking.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from '../components/Header.jsx';
+
+
+
+export default function Booking() {
+  return (
+      <div>
+      <Header />
+      
+
+
+
+      { <main className="container mx-auto p-4">
+        <h2 className="text-3xl font-semibold text-center mt-6">Welcome to The Lake & Resorts</h2>
+        <p className="text-gray-600 text-center mt-2">
+          Book your rooms, pools, and events easily with our online system.
+        </p>
+      </main> }
+
+      
+
+      
+
+    </div>
+    
+    
+  );
+}
