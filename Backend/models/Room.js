86d61f786cc10c1a0a81a8616ae2987e485c664d
Theekshan Chathuranga <theekshanacnb@gmail.com>
2025-07-09 18:41:10 +0530

    Revert "Add Hotel Reception Module to dev Branch"

--- Backend/models/Room.js
+++ Backend/models/Room.js
@@ -84,5 +84,8 @@ const roomSchema = new mongoose.Schema({
   toObject: { virtuals: true }
 });
 
+// Add index for better performance
+roomSchema.index({ roomNumber: 1 }, { unique: true });
+
 const Room = mongoose.model("Room", roomSchema);
 export default Room;
