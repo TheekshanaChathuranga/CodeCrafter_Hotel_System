68960c2f1b2b61dbfae9a6c27a815c2d84137325
Theekshana Chathuranga <theekshanacnb@gmail.com>
2025-07-09 19:59:11 +0530

    fix: Add missing route for Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -30,6 +30,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
